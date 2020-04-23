557d9d17f44206b3d9717321f4c5d53e53d87b5e
Michael Alvear-Pozo <michael.alvearpozo@ubalt.edu>
2020-04-22 20:17:44 -0400

    Initial Commit
    Commiting 1st time

---
+++ Zombie-Royale/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100F;
+
+    bool isDead = false;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <=0)
+        {
+            Die();
+        }
+    }
+ 
+    private void Die()
+    {
+        if (isDead) return;
+        isDead = true;
+        GetComponent<Animator>().SetTrigger("die");
+    }
+}
